20c8c29873207fa1921d37dd6267e26ac98dc4be
pbouchard110 <pbouchard110@gmail.com>
2024-01-09 18:07:26 -0500

    opengl changes- seem stable

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,14 +1,16 @@
 { pkgs,  ... }: {
   # hardware.video.hidpi.enable = true;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages = with pkgs; [
-    intel-media-driver
-    vaapiIntel
-    vaapiVdpau
-    libvdpau-va-gl
-    intel-compute-runtime
-  ];
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+ # hardware.opengl.extraPackages = with pkgs; [
+ #   intel-media-driver
+ #   vaapiIntel
+ #   vaapiVdpau
+ #   libvdpau-va-gl
+ #   intel-compute-runtime
+ # ];
+  };
   powerManagement.cpuFreqGovernor = "performance";
 }
