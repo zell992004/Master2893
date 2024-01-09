4f3b85cc420956d347f108a354b15dd85e5841ef
pbouchard110 <pbouchard110@gmail.com>
2024-01-09 18:54:22 -0500

    add g14 to core.default

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -46,4 +46,23 @@ in
       ++ [ (import ./../../hosts/T480/default.nix) ]
     ;
   };
+      G14 = nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit self inputs; };
+    modules =
+         [ nixos-hardware.nixosModules.asus-zephyrus-ga401]
+      ++ [ (import ./hardware.nix) ]
+      ++ [ (import ./steam.nix) ] 
+      ++ [ (import ./pipewire.nix) ]
+      ++ [ (import ./program.nix) ]
+      ++ [ (import ./security.nix) ]
+      ++ [ (import ./services.nix) ]
+      ++ [ (import ./system.nix) ]
+      ++ [ (import ./user.nix) ]
+      ++ [ (import ./wayland.nix) ]
+      ++ [ (import ./virtualisation.nix) ]
+      ++ [ (import ./obsidian.nix)]
+      ++ [ (import ./../../hosts/P72/hardware-configuration.nix) ]
+      ++ [ (import ./../../hosts/P72/default.nix) ]
+    ;
+  };
 }
