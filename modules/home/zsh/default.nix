9104a0555d0e5b30fccd0be788aed7cf7ccba562
pbouchard110 <pbouchard110@gmail.com>
2024-01-09 22:18:03 -0500

    cleaned zsh alias

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -24,17 +24,12 @@ in {
       n = "clear && neofetch";
       v = "nvim";
       record = "wf-recorder --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor -f $HOME/Videos/$(date +'%Y%m%d%H%M%S_1.mp4')";
-      ani = "bash $HOME/.local/bin/anime --dub";
-      ls = "exa --icons --long";
+      ls = "eza --icons --long";
       startbg = " nohup Master2893/backgrounds/swww_randomize.sh ~/Master2893/backgrounds > output.log 2>&1 &";
       # nixos
       ncg = "nix-collect-garbage && nix-collect-garbage -d && sudo nix-collect-garbage && sudo nix-collect-garbage -d && sudo rm /nix/var/nix/gcroots/auto/*";
-      nrs = "sudo nixos-rebuild switch --flake ~/Flakes/.#nixos";
+      nrs = "sudo nixos-rebuild switch --flake ~/Master2893/.#";
       fl = "cd ~/Flakes/ && v";
-
-      # python
-      piv = "python -m venv .venv";
-      psv = "source .venv/bin/activate";
     };
   };
   };
