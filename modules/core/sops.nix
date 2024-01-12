b4403330ec94fd9cf058993ce71d72b022c22ed6
pbouchard110 <pbouchard110@gmail.com>
2024-01-11 20:19:34 -0500

    1

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -4,7 +4,7 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ./../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   
   sops.age.keyFile = "/home/user/.config/sops/age/keys.txt";
