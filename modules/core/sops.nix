7bb74b010e765e4d27a36d799c19182922f553cf
pbouchard110 <pbouchard110@gmail.com>
2024-01-11 20:33:49 -0500

    revisit sops-nix github

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -7,7 +7,7 @@
   sops.defaultSopsFile = ./../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   
-  sops.age.keyFile = "/home/user/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/home/zell/.config/sops/age/keys.txt";
 
   sops.secrets.example-key = { };
   sops.secrets."myservice/my_subdir/my_secret" = {
