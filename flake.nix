9b8861ee238590164b3c182bd1daf68f1f52d1f9
Peter Bouchard <zell992004@gmail.com>
2024-02-03 11:09:14 -0500

    1

--- flake.nix
+++ flake.nix
@@ -12,6 +12,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+     inputs.nixvim = {
+    url = "github:nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # url = "github:nix-community/nixvim/nixos-23.05";
+
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   };
   
 
