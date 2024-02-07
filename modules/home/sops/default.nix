f8178c9feceae7323d8ab3e7dea10a0896eae4d5
Peter Bouchard <zell992004@gmail.com>
2024-02-06 20:39:16 -0500

    1

--- modules/home/sops/default.nix
+++ modules/home/sops/default.nix
@@ -11,8 +11,8 @@ in {
   options.modules.sops-nix = { enable = mkEnableOption "sops-nix";};
   config = mkIf cfg.enable {
     sops = {
-      age.keyfile = "/home/zell/.config/sops/age/keys.txt";
-      defaultSopsFile = ../../../secrets.yaml;
+      age.keyFile = "/home/zell/.config/sops/age/keys.txt";
+      defaultSopsFile = ../../../secrets/secrets.yaml;
       secrets.example-key = {};
       secrets."myservice/my_subdir/my_secret" = {};
     };
