5281b71703acff8e885d7954545cac5fff5c1207
Peter Bouchard <zell992004@gmail.com>
2024-02-06 19:49:50 -0500

    1

--- modules/home/sops/default.nix
+++ modules/home/sops/default.nix
@@ -6,12 +6,15 @@
 }:
 with lib;
 let 
-  cfg = config.modules.sops-nix
+  cfg = config.modules.sops-nix;
 in {
   options.modules.sops-nix = { enable = mkEnableOption "sops-nix";};
   config = mkIf cfg.enable {
     sops = {
       age.keyfile = "/home/zell/.config/sops/age/keys.txt";
       defaultSopsFile = ../../../secrets.yaml;
+      secrets.example-key = {};
+      secrets."myservice/my_subdir/my_secret" = {};
     };
-  }
+  };
+}
